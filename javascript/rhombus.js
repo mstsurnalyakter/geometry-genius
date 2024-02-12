bbc21ce9e1585f0cf68a013eb503db84b9b4e327
mstsurnalyakter <surnalyms@gmail.com>
2024-02-12 22:52:21 +0600

    Rhombus calculate added

---
+++ javascript/rhombus.js
@@ -0,0 +1,22 @@
+const rhombusBtn = document.getElementById("rhombusBtn");
+const rhombusDiagonal1 = document.getElementById("rhombus-diagonal1");
+const rhombusDiagonal2 = document.getElementById("rhombus-diagonal2");
+const rhombusArea= document.getElementById("rhombus-area");
+
+rhombusBtn.addEventListener("click", () => {
+  const rhombusDiagonal1Text = rhombusDiagonal1.value;
+  const rhombusDiagonal2Text = rhombusDiagonal2.value;
+
+  const diagonal1 = parseFloat(rhombusDiagonal1Text);
+  const diagonal2 = parseFloat(rhombusDiagonal2Text);
+
+   if (!diagonal1 || !diagonal2 || diagonal1 !== "number" || diagonal2 !== "number") {
+     return alert(`Please Provide valid value.`);
+   }
+
+  const area = 0.5 * diagonal1 * diagonal2;
+  rhombusArea.innerText = area;
+
+  rhombusDiagonal1.value = "";
+  rhombusDiagonal2.value = "";
+});
